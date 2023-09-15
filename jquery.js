dc3927141c653ebe7696621373106857894a94b1
sosally <thdud4143@gmail.com>
2023-09-15 18:20:48 +0900

    0915

---
+++ jquery.js
@@ -0,0 +1,11 @@
+// const gnb = $('.gnb');
+// gnb.hover(
+//     function(){
+//         $('.sub').css('display','block');
+//         $('header').addClass('on');
+//     },
+//     function(){
+//         $('.sub').css('display','none');
+//         $('header').removeClass('on');
+//     }
+// );

428229527a06e7765e7ac4d11a2b7f06fa0ddf79
sosally <thdud4143@gmail.com>
2023-09-21 16:57:22 +0900

    0921_1차

---
+++ js/vanilla.js
@@ -0,0 +1,9 @@
+  //popup close
+
+  let closeButton = document.getElementById("closeButton");
+  let popup = document.querySelector(".popup");
+
+  closeButton.addEventListener("click", function() {
+    popup.remove();
+      
+  });

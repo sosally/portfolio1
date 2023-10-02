2d6c5bd138b6419c419fd808a3cd9784a22e807a
sosally <thdud4143@gmail.com>
2023-10-02 16:23:07 +0900

    1002news

--- js/jquery.js
+++ js/jquery.js
@@ -10,17 +10,3 @@ gnb.hover(
     }
 );
 
-  //main_slide
-  $('.main_banner').bxSlider({
-    // nextSelector : '.main_slide_btn .main_next_btn_inner',
-    // prevSelector : '.main_slide_btn .main_prev_btn_inner',
-    mode : 'fade',
-    minSlides :1,
-    maxSlides: 1,
-    moveSlides:1,
-    // slideWidth: '100%',
-    // slideMargin: 10,
-    auto:true,
-    speed: 500,
-    pause : 5000
-  });
--- js/vanilla.js
+++ js/vanilla.js
@@ -8,25 +8,58 @@
       
   });
 
-  //main_slide
-$('.main_banner').bxSlider({
-    // nextSelector : '.main_slide_btn .main_next_btn_inner',
-    // prevSelector : '.main_slide_btn .main_prev_btn_inner',
-    mode : 'fade',
-    minSlides :1,
-    maxSlides: 1,
-    moveSlides:1,
-    // slideWidth: '100%',
-    // slideMargin: 10,
-    auto:true,
-    speed: 500,
-    pause : 5000
-  });
+//공간소개 이미지
+function changeImage(imagePath) {
+  const imageElement = document.getElementById("space_image");
+
+  imageElement.style.opacity = "0";
   
-  //gnb
-  // const depth1 = document.querySelectorAll('.gnb')
-  // const depth2 = document.querySelectorAll('.sub li a');
+  setTimeout(function() {
+    imageElement.src = imagePath;
+    imageElement.style.opacity = "1";
+    
+   },500);
+}
+
+//news
+// const icons = document.querySelectorAll('.fa-plus');
+
+document.querySelector('.news1').addEventListener("mouseover", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.transform = "translateY(-20px)";
+  this.style.boxShadow = "5px -5px 10px rgba(0,0,0,0.1), -5px -5px 10px rgba(0,0,0,0.1), 5px 5px 10px rgba(0,0,0,0.1), -5px 5px 10px rgba(0,0,0,0.1)";
   
-  // depth1.addEventListener("mouseover", function(){
-  //   depth2.style.display = 'block';
-  // })
+  // icons.forEach(function(icon) {
+  //   icon.style.transform = 'rotate(90deg)';
+  // });
+});
+
+document.querySelector('.news1').addEventListener("mouseout", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.transform = "";
+  this.style.boxShadow = "";
+});
+
+document.querySelector('.news2').addEventListener("mouseover", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.boxShadow = "5px -5px 10px rgba(0,0,0,0.1), -5px -5px 10px rgba(0,0,0,0.1), 5px 5px 10px rgba(0,0,0,0.1), -5px 5px 10px rgba(0,0,0,0.1)";
+  this.style.transform = "translateY(-20px)";
+});
+
+document.querySelector('.news2').addEventListener("mouseout", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.boxShadow = "";
+  this.style.transform = "";
+});
+
+document.querySelector('.news3').addEventListener("mouseover", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.boxShadow = "5px -5px 10px rgba(0,0,0,0.1), -5px -5px 10px rgba(0,0,0,0.1), 5px 5px 10px rgba(0,0,0,0.1), -5px 5px 10px rgba(0,0,0,0.1)";
+  this.style.transform = "translateY(-20px)";
+});
+
+document.querySelector('.news3').addEventListener("mouseout", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.boxShadow = "";
+  this.style.transform = "";
+});
